b36d9b1fa4dd71284ef2ab41462f1353e49220cf
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2023-08-19 12:51:17 -0400

    serve Elm app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tokio;
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    let serve_dir = ServeDir::new("www/assets")
+        .not_found_service(ServeFile::new("www/index.html"));
+
+    let app = Router::new()
+        .nest_service("/", ServeFile::new("www/index.html"))
+        .nest_service("/assets", serve_dir);
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
