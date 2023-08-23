0bf2a482a87fcf15728d45c8020b72d76c0aa91d
Axel Tetzlaff <axel.tetzlaff@shopify.com>
2023-08-23 13:40:28 -0400

    Add content to the Elm Hello World lesson

--- client/src/Lessons/Elm/HelloWorld.elm
+++ client/src/Lessons/Elm/HelloWorld.elm
@@ -17,14 +17,16 @@ indexHtml =
     , content = htmlIntroIndexHtml
     }
 
+
 mainElm =
     { filename = "Main.elm"
     , filetype = ElmFile
     , content = mainElmContent
     }
 
+
 mainElmContent =
-  """module Main exposing (main)
+    """module Main exposing (main)
 import Html exposing (..)
 
 main = text "Hello from Elm!"
@@ -32,10 +34,41 @@ main = text "Hello from Elm!"
 
 
 body =
-    """**some** info about HTML
+    """## Hello Elm 👋
 
-  - oh
-  - yeah
+The browser itself does not understand Elm. We have to use the _Elm compiler_ to _translate_ aka _compile_
+our Elm program to JavaScript.
+
+But with **just** JavaScript the browser still can't display anything. We have to give the browser our 
+_"single (HTML) page"_ of our _Single Page Application_.
+
+This page can then pull in our compiled JavaScript to replace/modify that page.
+
+And the files in this lesson are the minimal setup to achieve this.
+
+When you hit run we execute this command from the [Elm guide](https://guide.elm-lang.org/interop/#compiling-to-javascript)
+on your behalf. The result is a _new_ file `main.js` that contains the _compiled_ version of the `main.elm` file.
+
+In the `<head>` section of the `index.html` we _include_ that `main.js` and it's content gets loaded by the browser.
+
+Loading that code makes our app "available" to the current page but doesn't execute/do anything just yet.
+
+The magic \u{1FA84} happens in the second `<script>` block. Here we tell the browser to _execute_ or _call_ the `init` function
+of something that's called `Elm.Main` which has been made availbe through the inclusion of the `main.js`.
+
+When we call `init` we have to tell which part of the page we want to replace with the content of our Elm application.
+The `node: document.getElementById('myapp')` looks for an element with the `id` attribute with the value `myapp`.
+
+By "handing" this element over to the `Elm.Main.init` call we give Elm an "entry point" into our document and it replaces
+that element with the content of our Elm application.
+
+The `Main.elm` contains the simplest Elm application one can think of. It just displays a static text.
+
+## Exercise
+
+1. Elm works exclusively on the element we give it. Add other elements to the page to see how they interact!
+
+2. You can even use the same app multiple times! Add a new element with the `id="secondapp"` and call init a second time to replace that new container. 
   """
 
 
@@ -54,4 +87,3 @@ htmlIntroIndexHtml =
     </script>
   </body>
 </html>"""
-
